f1cb144599f6300e874437d12cee4efcd8512864
Danica Gazdić <danicagazdic@gmail.com>
2024-01-03 19:40:16 +0100

    Fixing some issues I overlooked

--- python/main.py
+++ python/main.py
@@ -9,6 +9,8 @@ from components.ds import run_ds
 from components.dms import run_dms
 from components.db import run_db
 from components.four_segment import run_4_segment
+from components.rgb import run_rgb
+from components.ir import run_ir
 from queue import Queue
 import paho.mqtt.publish as publish
 
@@ -21,6 +23,7 @@ except:
 
 light_queue = Queue()
 buzzer_queue = Queue()
+rgb_queue = Queue()
 settings = {}
 
 def run_user_input_threads(threads, stop_event):
@@ -32,6 +35,7 @@ def run_user_input_threads(threads, stop_event):
 def user_input_thread(stop_event):
     global light_queue
     global buzzer_queue
+    global rgb_queue
 
     while True:
         user_input = input()
@@ -39,6 +43,8 @@ def user_input_thread(stop_event):
             light_queue.put(user_input)
         if user_input == 'b':
             buzzer_queue.put(user_input)
+        else: 
+            rgb_queue.put(user_input)
         time.sleep(0.1)
         if stop_event.is_set():
             break
@@ -82,6 +88,8 @@ def run_all_ultrasonic(threads, stop_event):
 
 def run_display(threads, stop_event):
     run_4_segment(settings["B4SD"], threads, stop_event)
+    run_rgb(light_queue, settings["BRGB"], threads, stop_event)
+    run_ir(settings["BIR"], threads, stop_event)
 
 
 if __name__ == "__main__":
