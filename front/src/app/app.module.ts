a0f6398dd1227280192f423b72d59c701a5e2930
Danica Gazdić <danicagazdic@gmail.com>
2024-01-04 14:02:42 +0100

    Implemented initial look on frontend

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DhtComponent } from './dht/dht.component';
 import { HomeComponent } from './home/home.component';
+import { MaterialModule } from 'src/infrastructure/material/material.module';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { HomeComponent } from './home/home.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
